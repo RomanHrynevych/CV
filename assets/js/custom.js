88ac66020a322e3bfb218875e04184f7c53b0390
RomanHrynevych <hrynevych.romann@gmail.com>
2021-11-09 21:58:37 +0200

    drop v1.0

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,15 @@
 
 window.addEventListener("load", function (e) {
   document.querySelector(`.preloader-outer`).style.display = "none";
+
+  document.querySelector(`.prog-html`).style.width = "85%";
+  document.querySelector(`.prog-css`).style.width = "80%";
+  document.querySelector(`.prog-js`).style.width = "65%";
+  document.querySelector(`.prog-python`).style.width = "47%";
 });
+document
+  .querySelector(`.hamburger-menu`)
+  .addEventListener("click", function () {
+    document.querySelector(`header`).classList.toggle("pushed");
+    document.querySelector(`.bar`).classList.toggle("animate");
+  });
